8d19d481aeb35c295c70d6671f0b336facbd511f
wei fuchuan <fuchuan.wei@hotmail.com>
2018-10-14 11:26:51 +0800

    update

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import applyDecoratedDescriptor from '@babel/runtime/helpers/esm/applyDecoratedDescriptor'
 import initializerDefineProperty from '@babel/runtime/helpers/esm/initializerDefineProperty'
 
-Object.assign(babelHelpers, {
-  applyDecoratedDescriptor,
-  initializerDefineProperty,
-});
+if (global.babelHelpers)
+  Object.assign(babelHelpers, {
+    applyDecoratedDescriptor,
+    initializerDefineProperty,
+  });
 
 require('./src');
